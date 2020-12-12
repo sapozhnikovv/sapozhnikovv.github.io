8d767e54aca03407062ddcafa08d8d88d70acd6c
Kseniya-Osinina <kseniya.osinina.buh@gmail.com>
2020-12-12 22:44:21 +0300

    22-44

--- js/js.js
+++ js/js.js
@@ -1,14 +1,12 @@
-$(document).ready(function () {
-	var language = window.navigator ? (window.navigator.language ||
-									   window.navigator.systemLanguage ||
-									   window.navigator.userLanguage) : "en";
-	language = language.substr(0, 2).toLowerCase();
+var language = window.navigator ? (window.navigator.language ||
+								   window.navigator.systemLanguage ||
+								   window.navigator.userLanguage) : "en";
+language = language.substr(0, 2).toLowerCase();
 
-	if (!window.location.hostname.includes("192.168.") && 
-		!window.location.hostname.includes("localhost")){
-			if (language === "ru" && (location.href === "https://sapozhnikovv.github.io/" || location.href === "https://sapozhnikovv.github.io"))
-			{ 
-				location.href = "ru.html";
-			}
-	}
-});
+if (!window.location.hostname.includes("192.168.") && 
+	!window.location.hostname.includes("localhost")){
+		if (language === "ru" && (location.href === "https://sapozhnikovv.github.io/" || location.href === "https://sapozhnikovv.github.io"))
+		{ 
+			location.href = "ru.html";
+		}
+}

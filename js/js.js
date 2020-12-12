b8b5efcfba30ad2bcfadc4c8f88e86fb647466c1
Kseniya-Osinina <kseniya.osinina.buh@gmail.com>
2020-12-12 22:37:30 +0300

    22-37

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function () {
 	var language = window.navigator ? (window.navigator.language ||
 									   window.navigator.systemLanguage ||
 									   window.navigator.userLanguage) : "en";
@@ -11,3 +11,4 @@
 				location.href = "ru.html";
 			}
 	}
+});
